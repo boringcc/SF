14856331bd09ba5a509c8c5d4b3582fcc8903ad3
cc <823517665@qq.com>
2018-07-31 23:44:12 +0800

    快排é£非递归方式测试

--- src/SFShiYang/test.java
+++ src/SFShiYang/test.java
@@ -6,11 +6,50 @@ import java.util.*;
 
 public class test {
 
-    @Test
-    public void test1() {
-        Integer[] datas = new Integer[]{1, 2, 3, 4, 5};
-        List list = Arrays.asList(datas);
-        System.out.println(list.size());
+    public static void quickSort(int[] a,int start,int end){
+        Stack<Integer> stack = new Stack<Integer>();
+        if(start < end){
+            stack.push(end);
+            stack.push(start);
+            while (!stack.isEmpty()){
+                int l = stack.pop();
+                int r = stack.pop();
+                int index = partition(a,l,r);
+                if(l < index -1){
+                    stack.push(index-1);
+                    stack.push(l);
+                }
+                if(r > index + 1){
+                    stack.push(r);
+                    stack.push(index + 1);
+                }
+            }
+        }
+    }
+
+
+    public static int partition(int[] a,int low,int hi){
+        int pivot = a[low];
+        while (low < hi){
+            while (low < hi && a[hi] >= pivot){
+                hi--;
+            }
+            a[low] = a[hi];
+            while (low < hi && a[low] <= pivot){
+                low++;
+            }
+            a[hi] = a[low];
+        }
+        a[low] = pivot;
+        return low;
+    }
+
+    public static void main(String[] args) {
+        int[] a = {5,4,3,2,1};
+        quickSort(a,0,4);
+        for(int i = 0;i < 5;i++){
+            System.out.println(a[i]);
+        }
     }
 
 
